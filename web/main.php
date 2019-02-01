218afbd5d25a6214a975326b4dfe80a2626307e3
maxjamin <maxjamin@gmail.com>
2019-02-01 16:16:42 -0700

    chaning main

--- web/main.php
+++ web/main.php
@@ -37,6 +37,13 @@ catch (PDOException $ex)
 	  echo '<br/>';
 	}
 
+	$stmt = $db->prepare('SELECT * FROM Customer WHERE id=:id AND name=:name');
+	$stmt->bindValue(':id', $id, PDO::PARAM_INT);
+	$stmt->bindValue(':name', $name, PDO::PARAM_STR);
+	$stmt->execute();
+	$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
 ?>
 
 </body>
