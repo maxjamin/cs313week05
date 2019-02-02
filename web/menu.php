d9d8ff78517f214b47e2d202093221366cf9947f
maxjamin <maxjamin@gmail.com>
2019-02-02 15:22:55 -0700

    Testing main.php

--- web/menu.php
+++ web/menu.php
@@ -78,9 +78,9 @@ catch (PDOException $ex)
 				"</td><td>" . "<img src=$image width='150' height='150'>" .
 				"</td><td>" . $table['price'] .
 				"</td><td>" . 
-				"
+				"<form>
 					<input type='button' name='addToCart' value='Add to Cart'>
-				" . 
+				</from>" . 
 				'</td></tr>';
 		} ?>
 
