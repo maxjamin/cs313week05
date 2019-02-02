c9a9174052dc9a6a9967afe19e5c3eb04b4dff55
maxjamin <maxjamin@gmail.com>
2019-02-02 15:48:37 -0700

    Testing main.phddp

--- web/menu.php
+++ web/menu.php
@@ -83,7 +83,7 @@ catch (PDOException $ex)
 				"</td><td>" . "<img src=$image width='150' height='150'>" .
 				"</td><td>" . $table['price'] .
 				"</td><td>" . 
-				"<input type='button' name='addToCart' onclick='addToCart($id)' value='Add to Cart'>" . 
+				"<input type='button' name='addToCart'"?> php onclick='addToCart($id)'<?php"value='Add to Cart'>" . 
 				'</td></tr>';
 		} ?>
 
