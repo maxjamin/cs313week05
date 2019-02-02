a47405d30ed1891819605dab3a9285b354c7a10c
maxjamin <maxjamin@gmail.com>
2019-02-01 17:05:01 -0700

    chaning main

--- web/main.php
+++ web/main.php
@@ -28,9 +28,9 @@ try
 
    // set the resulting array to associative
    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-   /*foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+   foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
         echo $v;
-   }*/
+   }
 
 
 }
